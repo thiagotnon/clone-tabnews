c393ca53256a08e2d27d9087220ead4808ea0596
Thiago Natan <36268854+thiagotnon@users.noreply.github.com>
2023-09-20 21:14:06 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Carla, eu amo você! Se você me ama, dá uma risadinha. 🖤</h1>;
+  return <h1>Carla, eu amo você! Se você me ama, dá uma risadinha. 🖤</h1>;
 }
