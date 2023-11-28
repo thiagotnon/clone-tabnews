e6258a7009244d5f885d0b97dc745c7390a9319c
Thiago Natan <36268854+thiagotnon@users.noreply.github.com>
2023-11-28 23:14:41 +0000

    `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ chave: "alunos do curso.dev são acima da média" });
 }
+
+export default status;
